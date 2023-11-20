2c32bdadbfa8ef5a0a7a259c8c22c03d4ef98399
KOLEXCLEAM07 <quawwiyuogunleye@gmail.com>
2023-11-20 16:08:21 +0100

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
       <div className=" max-w-screen-lg mx-auto flex flex-col md:flex-row items-center justify-center h-full px-4">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Full Stack Developer
+            I'm a Front-end  Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
           I have a year plus experience in developing and designing websites.
