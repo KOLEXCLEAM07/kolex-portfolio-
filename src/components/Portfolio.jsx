cf92da54de3e6504101b721392c60767de496138
KOLEXCLEAM07 <quawwiyuogunleye@gmail.com>
2023-11-20 12:23:05 +0100

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import  arrayDestruct from'../assets/portfolio/arrayDestruct.jpg'
-import  navbar from'../assets/portfolio/navbar.jpg'
-import  reactParallax from'../assets/portfolio/reactParallax.jpg'
-import  reactSmooth from'../assets/portfolio/reactSmooth.jpg'
-import  usestae from'../assets/portfolio/usestate.jpg'
-import cssMediaQueries from'../assets/portfolio/cssMediaQueries.jpg'
+// import  arrayDestruct from'../assets/portfolio/arrayDestruct.jpg'
+// import  navbar from'../assets/portfolio/navbar.jpg'
+// import  reactParallax from'../assets/portfolio/reactParallax.jpg'
+// import  reactSmooth from'../assets/portfolio/reactSmooth.jpg'
+// import  usestae from'../assets/portfolio/usestate.jpg'
+// import cssMediaQueries from'../assets/portfolio/cssMediaQueries.jpg'
 import reactWeather from'../assets/portfolio/reactWeather.jpg'
 
 const Portfolio = () => {
