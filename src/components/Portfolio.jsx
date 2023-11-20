fc1644ce692f5df8661e17d58435dfd102b905a6
KOLEXCLEAM07 <quawwiyuogunleye@gmail.com>
2023-11-20 19:11:26 +0100

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import  arrayDestruct from'../assets/portfolio/arrayDestruct.jpg'
 import  navbar from'../assets/portfolio/navbar.jpg'
 import  reactParallax from'../assets/portfolio/reactParallax.jpg'
 import  reactSmooth from'../assets/portfolio/reactSmooth.jpg'
