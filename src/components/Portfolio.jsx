1241100fe24610117fee6cd1402a4cee278b88f8
KOLEXCLEAM07 <quawwiyuogunleye@gmail.com>
2023-11-20 20:25:14 +0100

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -55,15 +55,8 @@ const Portfolio = () => {
                     alt=""
                     className="rounded-md duration-200 hover:scale-105"
                   />
-                  <div className="flex items-center justify-center">
-                    <button className=" w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                      
-                    </button>
-                    <button className="px-6 py-3 w-1/2 m-4 duration-200 hover:scale-105">
-                      
-                    </button>
-                  </div>
-                </div>
+                  
+                </div> 
               ))}
             </div>
           </div>
