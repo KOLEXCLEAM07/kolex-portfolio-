fe4193a8d2c902f1488a7ea1919f1a199dbdb629
KOLEXCLEAM07 <quawwiyuogunleye@gmail.com>
2023-11-21 14:17:23 +0100

    Update Expierence.jsx

--- src/components/Expierence.jsx
+++ src/components/Expierence.jsx
@@ -61,38 +61,34 @@ const Expierence = () => {
             style:'shadow-gray-500'
         },
     ]
-
-
-  return (
-    <div name='experience'
-     className='  bg-gradient-to-b from-gray-800 to to-black w-full h-screen'>
-        <div 
-        className=' max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
-            <div className='  s'>
-                
-            <p className=' text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Experience</p>
-            <p className=' py-6'>This are the Programming languages i know and work with</p>
-
-        </div>  
-
-
-        <div className=' grid grid-cols-2 w-full sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
-
-
-            {
-                languages.map(({id,src,title,style})=>(
-             <div key={id} 
-             className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
-               <img className=' w-20 mx-auto ' src={src} alt="" /> 
-               <p className='mt-4'>{title}</p>
+    return (
+        <div
+          name="experience"
+          className="bg-gradient-to-b from-gray-800 to-black w-full h-screen"
+        >
+          <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
+            <div>
+              <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
+                Experience
+              </p>
+              <p className="py-6">These are the technologies I've worked with</p>
             </div>
-                ))}
-
-        </div>
-            </div >
-    </div>
     
-  )
-}
-
-export default Expierence
+            <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8  px-12 sm:px-0">
+              {languages.map(({ id, src, title, style }) => (
+                <div
+                  key={id}
+                  className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
+                >
+                  <img src={src} alt="" className="w-20 mx-auto" />
+                  <p className="mt-4">{title}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      );
+    };
+    
+    export default Expierence;
+    
