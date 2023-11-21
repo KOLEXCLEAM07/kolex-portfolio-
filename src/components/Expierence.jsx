28e79486cfde5fd1952c7c49b62158d4213c6997
KOLEXCLEAM07 <quawwiyuogunleye@gmail.com>
2023-11-21 14:11:10 +0100

    p

--- src/components/Expierence.jsx
+++ src/components/Expierence.jsx
@@ -70,7 +70,7 @@ const Expierence = () => {
         className=' max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
             <div className='  s'>
                 
-            <p className=' text-4xl font-bold border-b-4 border-gray-500 p-2 inline mt-9'>Experience</p>
+            <p className=' text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Experience</p>
             <p className=' py-6'>This are the Programming languages i know and work with</p>
 
         </div>  
